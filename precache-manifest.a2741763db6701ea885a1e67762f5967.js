417c09849e8f2a4cceb40294d880b3c8b5ef4c99
hoanganh24 <hoangngocanh2495@gmail.com.vn>
2020-05-10 19:13:54 +0700

    Updates

---
+++ precache-manifest.a2741763db6701ea885a1e67762f5967.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "879072ac74bbbcee93ca1b836cd07176",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71ac66f3cd301c850abd",
+    "url": "/static/css/main.a084775c.chunk.css"
+  },
+  {
+    "revision": "20cbbb82e07c7e1f4aa2",
+    "url": "/static/js/2.1160b9b6.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/static/js/2.1160b9b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71ac66f3cd301c850abd",
+    "url": "/static/js/main.08959e02.chunk.js"
+  },
+  {
+    "revision": "090fbb06d804952982c0",
+    "url": "/static/js/runtime-main.3f054f4f.js"
+  }
+]);
